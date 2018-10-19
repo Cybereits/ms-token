2061c20f5d8b803d569b83457c9ca049d3ab8f27
marsoln <marsoln1988@gmail.com>
2018-10-19 14:57:27 +0800

    nothing

--- server/common/auth.js
+++ server/common/auth.js
@@ -1,18 +1,21 @@
 export const sessionValidWrapper = (graphqlFieldsSchema) => {
   let __originFunc = graphqlFieldsSchema.resolve
+
   graphqlFieldsSchema.resolve = (root, _, ctx) => {
     let { session } = ctx
     if (session && session.admin) {
       return __originFunc(root, _, ctx)
     } else {
-      return new Error('Unauthorized!')
+      throw new Error('Unauthorized!')
     }
   }
+
   return graphqlFieldsSchema
 }
 
 export const authLevelWrapper = function (levels, graphqlFieldsSchema) {
   let __originFunc = graphqlFieldsSchema.resolve
+
   graphqlFieldsSchema.resolve = (root, _, ctx) => {
     let { session } = ctx
     if (session && session.admin && levels.indexOf(session.admin.role) > -1) {
@@ -21,5 +24,6 @@ export const authLevelWrapper = function (levels, graphqlFieldsSchema) {
       return new Error('您没有权限访问该接口')
     }
   }
+
   return graphqlFieldsSchema
 }
