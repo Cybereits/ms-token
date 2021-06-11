8f0e0e53f12a0134ac9adf077780f840c5760c40
marsoln <marsoln1988@gmail.com>
2021-06-11 13:52:00 +0400

    换成国外的 etherscan 地址

--- core/etherscan/index.js
+++ core/etherscan/index.js
@@ -2,12 +2,12 @@ import fetch from 'isomorphic-fetch'
 
 export const getGasPriceFromEtherscan = () =>
   fetch(
-    'https://api-cn.etherscan.com/api?module=gastracker&action=gasoracle&apikey=GKKVYS15XJBAWTRKK6BBKZ4BJSYCTDMXEB',
+    'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=GKKVYS15XJBAWTRKK6BBKZ4BJSYCTDMXEB',
   )
     .then((res) => res.json())
     .then((res) => {
       if (res.status === '1') {
-        const { FastGasPrice } = result
+        const { FastGasPrice } = res.result
         return (+FastGasPrice * 10 ** 9).toString()
       } else {
         throw new Error('从 etherscan 获取 gasPrice 失败')
